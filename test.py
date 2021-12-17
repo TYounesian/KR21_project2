dc4e5659da196fb665d774bb289475c320dfb194
TYounesian <t.yoonesian@gmail.com>
2021-12-17 17:02:20 +0100

    edge pruning

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from typing import Union
 from BayesNet import BayesNet
 import pdb
 import copy
+import pandas as pd
 
 class BNReasoner:
     def __init__(self, net: Union[str, BayesNet]):
@@ -60,27 +61,40 @@ class BNReasoner:
                     break
 
 
-    def Net_pruning(self, q, e):
+    def Net_pruning(self, q, e, e_value):
         """
-        Node and Endge pruning with the aim to keep P(Q|E) consistent
+        Node and Edge pruning with the aim to keep P(Q|E) consistent
         :param variables: set of given variables q and given evidence e
         """
 
         # Node Pruning:
         net_variables = self.bn.get_all_variables()
-        rest_variables = [var for vars in net_variables not in [q,e]]
+        rest_variables = [var for var in net_variables if var not in [q,e]]
 
         pruned_net = copy.deepcopy(self.bn)
+        pdb.set_trace()
         leaf_nodes = []
         for var in range(len(rest_variables)):
             children = self.bn.get_children(rest_variables[var])
             if children == []:
                 leaf_nodes.append(rest_variables[var])
+                #pruned_net.update_cpt(rest_variables[var], [])
                 pruned_net.del_var(rest_variables[var])
         
+        pruned_net.draw_structure()
+        # Edge Pruning
+        for var in range(len([e])):
+            vars_children = pruned_net.get_children([e][var])
+            for i in range(len(vars_children)):
+                pruned_net.del_edge([[e][var], vars_children[i]])
+                new_cpt = pruned_net.reduce_factor(pd.Series([[e_value][var]], [[e][var]]), pruned_net.get_cpt(vars_children[i]))
+                pruned_net.update_cpt(vars_children[i], new_cpt)
+
+        pruned_net.draw_structure()
         
 
 test_net = BNReasoner('testing/dog_problem.BIFXML')
 test_net.bn.draw_structure()
-test_net.dSep('bowel-problem', 'dog-out', 'family-out')
+#test_net.dSep('bowel-problem', 'dog-out', 'family-out')
+test_net.Net_pruning('hear-bark', 'bowel-problem', True)
 # pdb.set_trace()
